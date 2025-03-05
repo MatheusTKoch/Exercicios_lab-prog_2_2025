9446d985f4f71e3c6496473a1c9c6e13714a84f2
Matheus <matheustrilhakoch@hotmail.com>
2025-03-05 20:48:23 -0300

    Continuacao de exercicios de array

--- laboratorio/ExerciciosArrays.java
+++ laboratorio/ExerciciosArrays.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class ExerciciosArrays {
     static void exercicio1a() {
@@ -44,11 +47,22 @@ public class ExerciciosArrays {
         }
         System.out.println(num[0][0] + " " + num[0][1] + " " + num[0][2] + " " + num[0][3] + " " + num[0][4] + " " + num[0][5]);
     }
+
+    static void retornaMatriz(int tamMatriz) {
+        int[] matriz = new int[tamMatriz];
+        Random rand = new Random();
+        for (int i = 0; i < tamMatriz; i++) {
+            int numRandom = rand.nextInt(100);
+            matriz[i] = numRandom;
+        }
+
+        System.out.println(Arrays.toString(matriz));
+    }
                 
         public static void main(String[] args) {
-            exercicio1a();
-            exercicio1b();
-            exercicio1c();
-            exercicio1d();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite o tamanho da matriz: ");
+            int tamMatriz = scan.nextInt();
+            retornaMatriz(tamMatriz);
     }
 }
