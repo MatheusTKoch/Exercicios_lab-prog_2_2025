fe16e622d53a7d53252ab168ed8ec213151c07e5
Matheus <matheustrilhakoch@hotmail.com>
2025-03-05 15:54:12 -0300

    Adicionado pdf e exercicios

--- laboratorio/ExerciciosArrays.java
+++ laboratorio/ExerciciosArrays.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ExerciciosArrays {
     static void exercicio1a() {
         int[][] num = { {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} };
@@ -16,11 +18,37 @@ public class ExerciciosArrays {
                 System.out.print(i + " ");
             }
             System.out.println();
+        }
+    }
+
+    static void exercicio1c() {
+        int[][] num = new int[6][6];
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    num[i][j] = i;
+                    System.out.print(num[i][j] + " ");
+                }
+                    System.out.println();
+                }
+    }
+
+    static void exercicio1d() {
+        int[][] num = {{-1, -1, -1, -1, -1, -1}, {0,  0,  0,  0,  0,  0}};
+        for (int i = 0; i < 4; i++) {
+            for( int[] row : num) {
+                for (int j : row) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
             }
         }
-    
-    public static void main(String[] args) {
-        exercicio1a();
-        exercicio1b();
+        System.out.println(num[0][0] + " " + num[0][1] + " " + num[0][2] + " " + num[0][3] + " " + num[0][4] + " " + num[0][5]);
+    }
+                
+        public static void main(String[] args) {
+            exercicio1a();
+            exercicio1b();
+            exercicio1c();
+            exercicio1d();
     }
 }
