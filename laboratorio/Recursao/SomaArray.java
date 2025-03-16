89253a58335dd8925bb23c60fffefacab7bf90f2
Matheus <matheustrilhakoch@hotmail.com>
2025-03-15 23:11:59 -0300

    Continuacao de exercicios de recursao

--- laboratorio/Recursao/SomaArray.java
+++ laboratorio/Recursao/SomaArray.java
@@ -8,10 +8,18 @@ public class SomaArray {
         somaArray(array);
     }
 
+    public static int A(int i){
+        if(i > 10) return 0;
+        int x = 5 + i;
+        i += 3;
+        System.out.println((x + A(i)));
+        return x + A(i);
+    }
+
     private static void imprimeArray(double valor) {
         System.out.println("Soma dos valores do array: " + valor);
     }
-    
+
     private static double somaArray(double[][] array) {
         double soma = 0;
         for (int i = 0; i < array.length; i++) {
