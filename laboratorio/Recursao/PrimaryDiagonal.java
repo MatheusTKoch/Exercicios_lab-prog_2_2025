ef4c3123a20f24f70ac28dc99ed15575e6a410fe
Matheus <matheustrilhakoch@hotmail.com>
2025-03-19 22:31:02 -0300

    Exercicio de recursao

--- laboratorio/Recursao/PrimaryDiagonal.java
+++ laboratorio/Recursao/PrimaryDiagonal.java
@@ -1,9 +1,19 @@
 package Recursao;
 
 public class PrimaryDiagonal {
-    public static int printPrimaryDiagonal(int[][] m) throws IllegalArgumentException, MatrixNotPossibleException{
+    public static void validatePrimaryDiagonal(int[][] m) throws IllegalArgumentException, MatrixNotPossibleException{
         if(m == null) throw new IllegalArgumentException();
         if(m.length != m[0].length) throw new MatrixNotPossibleException();
-        return 0;
+        getPrimaryDiagonal(m, 0);
+    }
+
+    private static int[] getPrimaryDiagonal(int[][] m, int j) {
+        int[] primaryDiagonal = new int[m.length];
+        if(j == m.length) return primaryDiagonal;
+        for(int i = 0; i < m.length; i++){
+            primaryDiagonal[i] = m[i][i];
+            System.out.println(primaryDiagonal[i]);
+        }
+        return getPrimaryDiagonal(m, j + 1);
     }
 }
--- laboratorio/Recursao/Principal.java
+++ laboratorio/Recursao/Principal.java
@@ -1,9 +1,10 @@
 package Recursao;
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException, MatrixNotPossibleException {
         // ArrayInvertido.arrayInvertida(new int[]{1, 2, 3, 4, 5});
         // SomaArray.recebeArray(new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-        System.out.println(MenorPosicao.encontraPosicaoMenorValor(new int[]{23, 35, 16, 8, 13, 11, 10, 20, 9, 12}));
+        // MenorPosicao.encontraPosicaoMenorValor(new int[]{23, 35, 16, 8, 13, 11, 10, 20, 9, 12});
+        PrimaryDiagonal.validatePrimaryDiagonal(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
     }
 }
