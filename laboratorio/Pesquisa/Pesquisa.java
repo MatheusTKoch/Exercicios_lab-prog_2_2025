24fe92887e6a379b5388ce32fb46530563b00d5d
Matheus <matheustrilhakoch@hotmail.com>
2025-04-13 14:37:57 -0300

    Atv pesquisa/ordenacao

--- laboratorio/Pesquisa/Pesquisa.java
+++ laboratorio/Pesquisa/Pesquisa.java
@@ -1,11 +1,12 @@
 package Pesquisa;
 
 public class Pesquisa {
-    //Faça uma versão recursiva do método de Pesquisa Sequencial para arrays não-ordenados visto em aula. 
+    // Faça uma versão recursiva do método de Pesquisa Sequencial para arrays
+    // não-ordenados visto em aula.
     public static int pesquisaNum(int[] vetor, int num) {
         return pesquisaNum(vetor, num, 0);
     }
-    
+
     private static int pesquisaNum(int[] vetor, int num, int i) {
         if (i >= vetor.length) {
             return -1;
@@ -15,9 +16,22 @@ public class Pesquisa {
         }
         return pesquisaNum(vetor, num, i + 1);
     }
-    public static void main(String[] nums) {
-        int[] vet = {1, 2, 4, 5, 12, 1289, 1, 23};
-        System.out.println(pesquisaNum(vet, 23));
+
+    // Faça uma versão recursiva do método de Pesquisa Sequencial para arrays
+    // ordenados visto em aula.
+    public static int pesqSeqOrd(int[] vetor, int num) {
+        return pesqSeqOrd(vetor, num, 0);
     }
-    
+
+    private static int pesqSeqOrd(int[] vetor, int num, int i) {
+        if ((i < vetor.length) && (vetor[i] <= num) && (vetor[i] == num)) {
+            return i;
+        }  
+        return pesqSeqOrd(vetor, num, i + 1);
+    }
+    public static void main(String[] nums) {
+        int[] vet = { 1, 2, 4, 5, 12, 23, 43};
+        System.out.println(pesqSeqOrd(vet, 43));
+    }
+
 }
