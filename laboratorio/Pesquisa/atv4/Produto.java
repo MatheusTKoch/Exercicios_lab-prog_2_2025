33da559653c33d9e9bb97c2d33726425f42ebec0
Matheus <matheustrilhakoch@hotmail.com>
2025-04-14 21:39:52 -0300

    Adicionada classe produto atv4

--- laboratorio/Pesquisa/atv4/Produto.java
+++ laboratorio/Pesquisa/atv4/Produto.java
@@ -2,5 +2,42 @@
 package Pesquisa.atv4;
 
 public abstract class Produto {
-    
+    String nome;
+    String descricao;
+    double valor;
+
+    public Produto(String nome, String descricao, double valor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome do produto:" + getNome() + ", dados:" + getDescricao() + ", valor: R$" + getValor(); 
+    }
 }
