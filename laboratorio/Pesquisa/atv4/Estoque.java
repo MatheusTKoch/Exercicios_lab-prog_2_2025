4da7107eb64047efa657528781355bc56b332b84
Matheus <matheustrilhakoch@hotmail.com>
2025-04-17 08:32:06 -0300

    Adicionando metodo de ordenacao

--- laboratorio/Pesquisa/atv4/Estoque.java
+++ laboratorio/Pesquisa/atv4/Estoque.java
@@ -1,5 +1,5 @@
 // Crie uma classe chamada Estoque, que possui um array de Produto como atributo. No construtor de Estoque, receba o array de Produto. Crie os métodos de acesso e o toString para esta classe.
-// Na classe Estoque, crie um método chamado ordenaProdutosPorValor, que ordena o array de produtos por valor (do menor ao maior), utilizando Inserção Direta.
+
 package Pesquisa.atv4;
 
 public class Estoque {
@@ -20,5 +20,20 @@ public class Estoque {
     @Override
     public String toString() {
         return "Lista de produtos: " + getArrayProduto();
+    }
+
+    // Na classe Estoque, crie um método chamado ordenaProdutosPorValor, que ordena o array de produtos por valor (do menor ao maior), utilizando Inserção Direta.
+    public Produto[] ordenaProdutosPorValor() {
+        int n = arrayProduto.length;
+        for (int i = 1; i < n; i++) {
+            Produto key = arrayProduto[i];
+            int j = i - 1;
+            while (j >= 0 && arrayProduto[j].getValor() > key.getValor()) {
+                arrayProduto[j + 1] = arrayProduto[j];
+                j = j - 1;
+            }
+            arrayProduto[j + 1] = key;
+        }
+        return arrayProduto;
     }
 }
