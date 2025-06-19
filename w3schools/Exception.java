78cb97ae397c4900aef48239e72556d72a42ccc0
MatheusTKoch <matheust_koch@hotmail.com>
2025-06-18 21:07:05 -0300

    Implementação do método checkFile para leitura de arquivos e tratamento de exceção FileNotFoundException; ajuste no método main para chamar checkFile.

--- w3schools/Exception.java
+++ w3schools/Exception.java
@@ -1,5 +1,10 @@
 package w3schools;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Exercicios retirados de https://www.w3resource.com/java-exercises/exception/index.php
 public class Exception {
     //Write a Java program to create a method that takes an integer as a parameter and throws an exception if the number is odd.
     public static void checkInt(int num) throws OddException {
@@ -16,8 +21,26 @@ public class Exception {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+    //Write a Java program to create a method that reads a file and throws an exception if the file is not found.
+    public static void checkFile(String path) {
+        try {
+            readFile(path);  
+        } catch (FileNotFoundException e) {
+            System.out.println("Error - " + e.getMessage());;
+        }
+    }
+
+    public static void readFile(String path) throws FileNotFoundException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String data = scanner.nextLine();
+            System.out.println(data);
+        }
+        scanner.close();
+    }
     public static void main(String[] args) {
-        int numTest = 6;
-        checkNum(numTest);
+        checkFile("test.txt");
     }
 }
