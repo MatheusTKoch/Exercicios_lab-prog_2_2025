09365020c192aaff01fee5c7242deea6d7a6b9fa
MatheusTKoch <matheust_koch@hotmail.com>
2025-06-18 23:36:04 -0300

    Implementação da classe Search e método binarySearch para busca binária em um array.

---
+++ w3schools/Search.java
@@ -0,0 +1,37 @@
+//Exercicios retirados de https://www.w3resource.com/java-exercises/search/index.php
+package w3schools;
+
+public class Search {
+    //Write a Java program to find a specified element in a given array of elements using Binary Search.
+    
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
+        int target = 10;
+        
+        int result = binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
