00163437140cd600b4416f3f3ed288234a36e227
MatheusTKoch <matheust_koch@hotmail.com>
2025-06-22 19:21:43 -0300

    Implementação do método insertionSort para ordenação de um array utilizando o algoritmo Insertion Sort; ajuste no método main para chamar insertionSort.

--- w3schools/Sorting.java
+++ w3schools/Sorting.java
@@ -60,10 +60,27 @@ public class Sorting {
         }
     }
 
+    //Write a Java program to sort an array of given integers using the Insertion sort algorithm.
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int k = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > k) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = k;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
         
-        selectionSort(arr);
+        insertionSort(arr);
 
         System.out.println("Sorted array: ");
         for (int value : arr) {
